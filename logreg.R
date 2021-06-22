98185ac3657a1028e7403273f6a4a9d707fe235e
Muhammad Ghazy <muhammadghazismp85@gmail.com>
2021-06-22 23:50:45 +0700

    Added Files

---
+++ logreg.R
@@ -0,0 +1,98 @@
+library(MASS)
+library(ggplot2)
+
+df_final <- read.csv("D:/Ghazi/Kuliah/Semester 6/MetLit/TA/df_final.csv")
+df_final_onehot <- read.csv("D:/Ghazi/Kuliah/Semester 6/MetLit/TA/df_final_onehot.csv")
+
+
+#Raw 
+df_final_raw <- read.csv("D:/Ghazi/Kuliah/Semester 6/MetLit/TA/df_raw_updated.csv")
+df_final_raw_deleted <- read.csv("D:/Ghazi/Kuliah/Semester 6/MetLit/TA/df_raw_updated_deleted.csv")
+dfr = df_final_raw_deleted
+
+dfr$gender = as.factor(dfr$gender)
+dfr$job = as.factor(dfr$job)
+dfr$phone_dur = as.factor(dfr$phone_dur)
+dfr$app_dur = as.factor(dfr$app_dur)
+dfr$h_w_inc = as.factor(dfr$h_w_inc)
+dfr$education = as.factor(dfr$education)
+dfr$earning = as.factor(dfr$earning)
+dfr$get = as.factor(dfr$get)
+
+dfr$photo_quan = as.integer(dfr$photo_quan)
+dfr$age = as.integer(dfr$age)
+dfr$height = as.integer(dfr$height)
+dfr$weight = as.integer(dfr$weight)
+
+str(dfr)
+xtabs(~get + gender , data=dfr)
+xtabs(~get + job , data=dfr)
+xtabs(~get + phone_dur , data=dfr)
+xtabs(~get + app_dur , data=dfr)
+xtabs(~get + h_w_inc , data=dfr)
+xtabs(~get + education , data=dfr)
+xtabs(~get + earning , data=dfr)
+
+
+#Full model
+logistic <- glm(get~.,family = binomial(link="logit"),data=dfr)
+
+summary(logistic)
+
+ll.null <- logistic$null.deviance/-2
+ll.proposed <- logistic$deviance/-2
+logmcfaddenr2 = ((logistic$null.deviance/-2) - (logistic$deviance/-2)) / (logistic$null.deviance/-2)
+logmcfaddenr2
+pval = 1 - pchisq(2*(ll.proposed - ll.null), df=(length(logistic$coefficients)-1))
+pval
+
+#Model kita
+modelkita = glm(get~gender+age+height+weight+photo_quan+education,data = dfr,family = binomial((link='logit')))
+summary(modelkita)
+
+#Stepwise moodel
+step = stepAIC(logistic,direction = 'both',trace=FALSE)
+step
+summary(step)
+step_r2 = ((step$null.deviance/-2) - (step$deviance/-2)) / (step$null.deviance/-2)
+step_r2
+
+#Final model
+finalmodel = glm(get~app_dur,family = binomial(link='logit'),data=dfr)
+summary(finalmodel)
+finalr2 = ((finalmodel$null.deviance/-2) - (finalmodel$deviance/-2)) / (finalmodel$null.deviance/-2)
+finalr2
+
+########## plotting model full
+predicted.data <- data.frame(
+  probability.of.dapet=logistic$fitted.values,
+  get=dfr$get)
+
+predicted.data <- predicted.data[order(predicted.data$probability.of.dapet, decreasing=FALSE),]
+
+predicted.data$rank <- 1:nrow(predicted.data)
+
+predicted.data
+
+ggplot(data=predicted.data, aes(x=rank, y=probability.of.dapet)) +
+  geom_point(aes(color=get), alpha=1, shape=10, stroke=2) +
+  xlab("Index") +
+  ylab("Predicted probability of getting partner")
+
+
+############ plotting model satunya lagi
+predicted.data.step <- data.frame(
+  probability.of.dapet=step$fitted.values,
+  get=dfr$get)
+
+predicted.data.step <- predicted.data.step[
+  order(predicted.data.step$probability.of.dapet, decreasing=FALSE),]
+predicted.data.step$rank <- 1:nrow(predicted.data.step)
+
+predicted.data.step
+
+ggplot(data=predicted.data.step, aes(x=rank, y=probability.of.dapet)) +
+  geom_point(aes(color=get), alpha=1, shape=4, stroke=2) +
+  xlab("Index") +
+  ylab("Predicted probability of getting partner")
+
